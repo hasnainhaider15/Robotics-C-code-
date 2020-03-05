c6aed226b25c98c79670a176070e51d6d7d0bcdf
Hasnain Haider <hasnainhaider15@gmail.com>
2020-03-05 12:35:23 +0500

    Small program to control the sped of left and right motors to a robot to move using arrow keys

---
+++ Motor Control System/Motors_Control.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include <conio.h>
+using namespace std;
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+int main()
+{
+	
+	int lm = 0, rm = 0;
+    int c = 0;
+    cout<<"Speed: "<<"Left Motor: "<<lm<<" Right Motor: "<<rm<<endl;
+    while(true)
+    {
+		
+        switch((c=getch())) {
+        case KEY_UP:
+            //cout << endl << "Up" << endl;//key up
+            if(lm != rm)
+            {
+            	if(lm>rm)
+            	{
+            		lm = rm;	
+				}
+				else
+				{
+					rm = lm;
+				}
+			}
+            lm = lm +5;
+            rm = rm +5;
+            cout<<"Speed: "<<"Left Motor: "<<lm<<" Right Motor: "<<rm<<endl;
+            break;
+        case KEY_DOWN:
+            //cout << endl << "Down" << endl;   // key down
+             if(lm != rm)
+            {
+            	if(lm>rm)
+            	{
+            		lm = rm;	
+				}
+				else
+				{
+					rm = lm;
+				}
+			}
+            lm = lm -5;
+            rm = rm -5;
+            cout<<"Speed: "<<"Left Motor: "<<lm<<" Right Motor: "<<rm<<endl;
+            break;
+        case KEY_LEFT:
+            //cout << endl << "Left" << endl;  // key left
+            lm = lm -5;
+            rm = rm+5 ;
+            cout<<"Speed: "<<"Left Motor: "<<lm<<" Right Motor: "<<rm<<endl;
+            break;
+        case KEY_RIGHT:
+            //cout << endl << "Right" << endl;  // key right
+            lm = lm+5;
+            rm = rm-5 ;
+            cout<<"Speed: "<<"Left Motor: "<<lm<<" Right Motor: "<<rm<<endl;
+            break;
+        default:
+            //cout << endl << "null" << endl;  // not arrow
+            break;
+        }
+
+    }
+
+    return 0;
+}
